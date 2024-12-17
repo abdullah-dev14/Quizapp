b22622abac5f3ae02f90077b235736cd8c8542d4
Syed Muhammad Abdullah <syedmuhammadusman301@gmail.com>
2024-12-17 11:00:53 +0500

    Add files via upload

---
+++ Github/index.js
@@ -0,0 +1,48 @@
+let allQuestions = [
+    {
+        question: "CPU stand for?",
+        options: [
+            { answer: "Central Processing Unit", incorrect: true },
+            { answer: "Arithmetic Logic Unit", incorrect: false },
+            { answer: "Random Access Memory", incorrect: false },
+            { answer: "Memory Unit", incorrect: false },
+        ]
+    },
+    {
+        question: "JS stand for?",
+        options: [
+            { answer: "Java Script", incorrect: true },
+            { answer: "Hyper Text Markup Language", incorrect: false },
+            { answer: "Cascading Style Sheet", incorrect: false },
+            { answer: "Bootstrap", incorrect: false },
+        ]
+    },
+    {
+        question: "HTML stand for?",
+        options: [
+            { answer: "Hyper Text Markup Language", incorrect: true },
+            { answer: "Java Script", incorrect: false },
+            { answer: "Cascading Style Sheet", incorrect: false },
+            { answer: "Bootstrap", incorrect: false },
+        ]
+    },
+    {
+        question: "CCS stand for?",
+        options: [
+            { answer: "Cascading Style Sheet", incorrect: true },
+            { answer: "Hyper Text Markup Language", incorrect: false },
+            { answer: "Java Script", incorrect: false },
+            { answer: "Bootstrap", incorrect: false },
+        ]
+    },
+    {
+        question: "RAM stand for?",
+        options: [
+            { answer: "Random Access Memory", incorrect: true },
+            { answer: "Hyper Text Markup Language", incorrect: false },
+            { answer: "Cascading Style Sheet", incorrect: false },
+            { answer: "Bootstrap", incorrect: false },
+        ]
+    },
+]
+console.log(allQuestion);
